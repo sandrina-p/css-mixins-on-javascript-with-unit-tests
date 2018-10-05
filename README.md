504010ed273796892a0b66874d45e0ebeb8e95de
Sandrina Pereira <sandrina.pereira@farfetch.com>
2018-10-05 19:46:00 +0100

    Add CSS mixin example: media queries

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CSS Mixins on JS with Unit Tests
 
-A working example about the article I wrote:  
+A working example about the article I've written:  
 [Power-up your CSS Mixins by writing them with Javascript](https://medium.com/p/825b41728a55/).
 
 
@@ -8,17 +8,12 @@ A working example about the article I wrote:
 - Faster compilation time
 - Prettier and advanced logic than the @if | @for | $list
 - Access to values outside the CSS files
-- Unit testing — yes, put your CSS at proof!
+- Unit testing — yes, put your CSS to the proof!
 
 Built with [Webpack 2](https://webpack.js.org/), [Babel](https://babeljs.io/), [PostCSS](http://postcss.org/) and [Jest](https://facebook.github.io/jest/)
 
-## Install
+## Usage
 
 - `npm install`
-- `npm start` -
-- Go to http://localhost:3000/
+- `npm start` - Demo at http://localhost:3000/
 - `npm test` - run tests with Jest
-- `npm test` - create /build folder
-
-
-Make sure you have NodeJs and Webpack 2 installed globally.
--- src/styles/mixins/index.js
+++ src/styles/mixins/index.js
@@ -2,29 +2,45 @@ const paddingSizes = {
     sm: '0.2rem 0.5rem',
     md: '0.5rem 1rem',
     lg: '1rem 2rem',
-}
-const mixins = {
+};
 
-    // The first argument, mixin, is used to connect postCSS-mixins
-    button(mixin, bg, color, bgHover) {
-        return {
-            background: bg,
-            color: color,
-            '&:hover, &:focus': {
-                background: bgHover,
-            }
-        };
-    },
-    padding(mixin, size) {
-        return {
-            padding: paddingSizes[size],
-        }
-    },
-    fontSize(mixin, px) {
-        return {
-            'font-size': (px / 16) + 'rem',
-        }
+const breakpoints = {
+  sm: '20em',
+  md: '60em',
+  lg: '80em',
+};
+
+function padding(mixin, size) {
+    return {
+        padding: paddingSizes[size],
     }
+}
+
+function button(mixin, bg, color, bgHover) {
+    return {
+        background: bg,
+        color: color,
+        padding: '0.5rem',
+        '&:hover, &:focus': {
+            background: bgHover,
+        },
+    };
+}
+
+function fontSize(mixin, px) {
+    const pxNumb = px * 1 // convert String to Number
+    return {
+        'font-size': (pxNumb / 16) + 'rem',
+        [`@media (min-width: ${breakpoints.md})`]: {
+            'font-size': ((pxNumb + 2) / 16) + 'rem',
+        },
+    };
+}
+
+const mixins = {
+    padding,
+    button,
+    fontSize
 };
 
 module.exports = mixins;
